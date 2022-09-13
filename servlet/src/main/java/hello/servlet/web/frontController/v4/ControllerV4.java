6ea0e12bf904b9bf2c789810b1aeaddf357d00a2
임동혁 <idh1007@naver.com>
2022-09-13 22:30:38 +0900

    mvc

---
+++ servlet/src/main/java/hello/servlet/web/frontController/v4/ControllerV4.java
@@ -0,0 +1,15 @@
+package hello.servlet.web.frontController.v4;
+
+import hello.servlet.web.frontController.ModelView;
+
+import java.util.Map;
+
+public interface ControllerV4 {
+    /**
+     *
+     * @param paramMap
+     * @param model
+     * @return
+     */
+    String process(Map<String,String> paramMap, Map<String, Object> model);
+}
