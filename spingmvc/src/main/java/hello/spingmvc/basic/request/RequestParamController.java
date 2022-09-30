f646871415f7071b7b0fb51e3d84ab8a0d69bddd
임동혁 <idh1007@naver.com>
2022-09-30 22:20:30 +0900

    mvc - ModelAttribute

--- spingmvc/src/main/java/hello/spingmvc/basic/request/RequestParamController.java
+++ spingmvc/src/main/java/hello/spingmvc/basic/request/RequestParamController.java
@@ -1,12 +1,14 @@
 package hello.spingmvc.basic.request;
 
+import hello.spingmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -21,5 +23,90 @@ public class RequestParamController {
         log.info("age = {}", age);
 
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody //"ok"를 보내준다. 뷰리졸버를 찾지 않고 ResponseController랑 같은 역할
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge
+            //request.getParameter 랑 같은 역할
+    ){
+        log.info("username = {}", memberName);
+        log.info("age = {}", memberAge);
+
+        return "ok";
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age
+    ) {
+        log.info("username = {}", username);
+        log.info("age = {}", age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        //String int Integer 과 같은 단순타입이면 @RequestParam 생략 가능
+        log.info("username = {}", username);
+        log.info("age = {}", age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true)//username 이 꼭 들어와야함 없으면 오류난다
+            String username,
+            @RequestParam(required = false)
+            Integer age) {//int 는 null이 안들어가지만 Integer 는 객체 형태이기 때문에 null 이 들어갈 수 있다.
+        log.info("username = {}", username);
+        log.info("age = {}", age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            //defaultValue가 들어가면 required가 의미가 없어진다.
+            //defaultValue는 빈문자의 경우에도 기본 값으로 넣어준다
+            @RequestParam(required = true,defaultValue = "guest") String username,
+            @RequestParam(required = false,defaultValue = "-1") int age) {
+        log.info("username = {}", username);
+        log.info("age = {}", age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String,Object> paramMap) {
+        log.info("username = {}", paramMap.get("username"));
+        log.info("age = {}", paramMap.get("age"));
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+//    public String modeAttributeV1(@RequestParam String username, @RequestParam int age){
+    public String modeAttributeV1(@ModelAttribute HelloData helloData){
+//        HelloData helloData = new HelloData();
+//        helloData.setUsername(username);
+//        helloData.setAge(age);
+        //@ModelAttribute 가 위 주석 역할을 다 수행해준다....
+        log.info("username = {}", helloData.getUsername());
+        log.info("age = {}", helloData.getAge());
+
+        return "ok";
     }
 }
--- spingmvc/src/main/java/hello/spingmvc/basic/requestmapping/MappingController.java
+++ spingmvc/src/main/java/hello/spingmvc/basic/requestmapping/MappingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 public class MappingController {
     private Logger log = LoggerFactory.getLogger(MappingController.class);
 
+    //method 지정 안하면 어떤 method 로 호출 해도 실행된다.
     @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
     public String mappingGetV1(){
         log.info("hello basic");
@@ -31,6 +32,16 @@ public class MappingController {
     @GetMapping("/mapping/users/{userId}/oders/{orderId}")
     public String mappingPath(@PathVariable String userId,@PathVariable String orderId){
         log.info("mappingPath userId = {}, orderId = {}",userId,orderId);
+
+        return "ok";
+    }
+
+    //json 형식이어야만 요청이 된다.
+    //consume 은 요청 헤더의 컨텐트 타입
+    //produce는 요청 헤더의 어셉트 기반으로 매핑
+    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    public String mappingConsume(){
+        log.info("mapping consume");
 
         return "ok";
     }
