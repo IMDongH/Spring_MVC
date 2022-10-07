e91adc8591a3e7cf0a4067d07aeed49ee7a460a7
임동혁 <idh1007@naver.com>
2022-10-07 15:37:52 +0900

    mvc2

---
+++ thyme-leaf-basic/src/main/java/hello/thyme/leafbasic/ThymeLeafBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thyme.leafbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafBasicApplication.class, args);
+	}
+
+}
---
+++ thyme-leaf-basic/src/test/java/hello/thyme/leafbasic/ThymeLeafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thyme.leafbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeLeafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
