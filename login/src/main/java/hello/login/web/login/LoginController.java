888d1ba6d6476760c5be9d9ba434b175c7d3d3cb
임동혁 <idh1007@naver.com>
2022-10-18 19:18:36 +0900

    [ADD] MVC2 login 세션 직접 구현 적용

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -2,6 +2,7 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,20 +12,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @RequiredArgsConstructor
 @Controller
 @Slf4j
 public class LoginController {
 
     private final LoginService loginService;
-
+    private final SessionManager sessionManager;
     @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm") LoginForm form){
         return "login/loginForm";
     }
 
-    @PostMapping("/login")
-    public String login(@Validated @ModelAttribute LoginForm form , BindingResult bindingResult){
+//    @PostMapping("/login")
+    public String login(@Validated @ModelAttribute LoginForm form , BindingResult bindingResult, HttpServletResponse response){
         if(bindingResult.hasErrors()){
             return "login/loginForm";
         }
@@ -35,8 +40,45 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        //로그인 성공 처리 TODO
+        //로그인 성공 처리
+
+        //쿠키에 시간 정보를 주지 않으면 세션쿠키(브라우저 종료 시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
 
         return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String loginV2(@Validated @ModelAttribute LoginForm form , BindingResult bindingResult, HttpServletResponse response){
+        if(bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if(loginMember == null){ //global 오류
+            bindingResult.reject("loginFail","아이디 또는 비밀번호가 잘못되었습니다.");
+            return "login/loginForm";
+        }
+
+        //로그인 성공 처리
+
+        sessionManager.createSession(loginMember,response);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request){
+//        expireCookie(response, "memberId");
+        sessionManager.expire(request);
+        return "redirect:/";
+    }
+
+    private static void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
     }
 }
