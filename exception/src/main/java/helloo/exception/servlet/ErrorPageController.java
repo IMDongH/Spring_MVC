d3f80039dbfc6016bdd59593453ab3934cd521e9
임동혁 <idh1007@naver.com>
2022-10-25 22:28:14 +0900

    [ADD] MVC2 - 서블릿 예외 처리

---
+++ exception/src/main/java/helloo/exception/servlet/ErrorPageController.java
@@ -0,0 +1,27 @@
+package helloo.exception.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Controller
+public class ErrorPageController { // 오류 발생 시 화면 출력을 위한 컨트롤러
+
+    @RequestMapping("/error-page/404")
+    public String errorPage404(HttpServletRequest request, HttpServletResponse response){
+        log.info("errorPage 404");
+        return "error-page/404";
+    }
+
+    @RequestMapping("/error-page/500")
+    public String errorPage500(HttpServletRequest request, HttpServletResponse response){
+        log.info("errorPage 500");
+        return "error-page/500";
+    }
+
+
+}
