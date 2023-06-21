af4f31c132a001d41e625f11cb3305c901b23107
임동혁 <idh1007@naver.com>
2023-06-21 18:42:23 +0900

    DB1 - transaction

--- jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
+++ jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -4,5 +4,5 @@ package hello.jdbc.connection;
 public abstract class ConnectionConst {
     public static final String URL = "jdbc:mysql://localhost:3306/test";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "";
+    public static final String PASSWORD = "ehdgur123";
 }
