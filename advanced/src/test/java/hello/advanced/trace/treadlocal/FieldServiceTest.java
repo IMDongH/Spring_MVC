6f909a0fc6141d289e0685f0721cf20e321404b4
임동혁 <idh1007@naver.com>
2023-06-28 16:42:03 +0900

    spring 고급 - 쓰레드 동시성 문제

---
+++ advanced/src/test/java/hello/advanced/trace/treadlocal/FieldServiceTest.java
@@ -0,0 +1,58 @@
+package hello.advanced.trace.treadlocal;
+
+import hello.advanced.trace.treadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+
+    @Test
+    void field(){
+        log.info("main start");
+
+//        Runnable userA = new Runnable() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        };
+        /**
+         * 위 코드와 같은 코
+         */
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000); //동시성 문제 발생하지 않는 코드
+        sleep(100); //동시성 문제 발생하는 코드
+        threadB.start();
+
+        sleep(2000); //메인 쓰레드 종료 대기
+
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try{
+            Thread.sleep(millis);
+        } catch (InterruptedException e ){
+            e.printStackTrace();
+        }
+    }
+}
