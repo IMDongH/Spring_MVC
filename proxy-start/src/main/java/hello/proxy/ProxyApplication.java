4e7360b1b9952fe09cb0cdf99a7eb35d3ef95331
임동혁 <idh1007@naver.com>
2023-07-19 17:14:50 +0900

    spring 고급 - 프록시 패턴

--- proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,9 @@ package hello.proxy;
 
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
+import hello.proxy.config.v6_aop.AopConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 
-@Import(ProxyFactoryConfigV1.class)
+@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 //컴포넌트 스캔의 위치를 지정했는데 이 이유는 config 파일을 바꿔가기위함
 public class ProxyApplication {
